a7b43fdd9f7d3482027ad471617fad2467a9b4b1
jmplz14 <jmplz14@correo.ugr.es>
2018-12-03 13:43:39 +0100

    Introducido reconocimiento de manos y botones de ejemplo

---
+++ KinectButton/KinectButton/MainWindow.xaml.cs
@@ -0,0 +1,435 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+//using Microsoft.Office.Interop.PowerPoint;
+using System.IO;
+
+using Microsoft.Kinect;
+using Coding4Fun.Kinect.Wpf;
+using Microsoft.Kinect.Toolkit.Interaction;
+
+namespace KinectButton
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private KinectSensor _Kinect;
+        private WriteableBitmap _ColorImageBitmap;
+        private Int32Rect _ColorImageBitmapRect;
+        private int _ColorImageStride;
+        private Skeleton[] FrameSkeletons;
+        private InteractionStream interactionStream;
+        List<Button> buttons;
+        static Button selected;
+
+        float handX;
+        float handY;
+        String imagenMano;
+        bool cuadroSeleccionado;
+        private UserInfo[] userInfos;
+
+        public class DummyInteractionClient : IInteractionClient
+        {
+            public InteractionInfo GetInteractionInfoAtLocation(
+                int skeletonTrackingId,
+                InteractionHandType handType,
+                double x,
+                double y)
+            {
+                var result = new InteractionInfo();
+                result.IsGripTarget = true;
+                result.IsPressTarget = true;
+                result.PressAttractionPointX = 0.5;
+                result.PressAttractionPointY = 0.5;
+                result.PressTargetControlId = 1;
+
+                return result;
+            }
+        }
+
+        /*Microsoft.Office.Core.MsoTriState ofalse = Microsoft.Office.Core.MsoTriState.msoFalse;
+        Microsoft.Office.Core.MsoTriState otrue = Microsoft.Office.Core.MsoTriState.msoTrue;
+        private Microsoft.Office.Interop.PowerPoint.Application pptApp;*/
+
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            InitializeButtons();
+            kinectButton.Click += new RoutedEventHandler(kinectButton_Click);
+
+            this.Loaded += (s, e) => { DiscoverKinectSensor(); };
+            this.Unloaded += (s, e) => { this.Kinect = null; };
+        }
+
+        //initialize buttons to be checked
+        private void InitializeButtons()
+        {
+            buttons = new List<Button> { button1};
+        }
+
+        //raise event for Kinect sensor status changed
+        private void DiscoverKinectSensor()
+        {
+            KinectSensor.KinectSensors.StatusChanged += KinectSensors_StatusChanged;
+            this.Kinect = KinectSensor.KinectSensors.FirstOrDefault(x => x.Status == KinectStatus.Connected);
+        }
+
+        private void KinectSensors_StatusChanged(object sender, StatusChangedEventArgs e)
+        {
+            switch (e.Status)
+            {
+                case KinectStatus.Connected:
+                    if (this.Kinect == null)
+                    {
+                        this.Kinect = e.Sensor;
+                    }
+                    break;
+                case KinectStatus.Disconnected:
+                    if (this.Kinect == e.Sensor)
+                    {
+                        this.Kinect = null;
+                        this.Kinect = KinectSensor.KinectSensors.FirstOrDefault(x => x.Status == KinectStatus.Connected);
+                        if (this.Kinect == null)
+                        {
+                            MessageBox.Show("Sensor Disconnected. Please reconnect to continue.");
+                        }
+                    }
+                    break;
+            }
+        }
+
+        public KinectSensor Kinect
+        {
+            get { return this._Kinect; }
+            set
+            {
+                if (this._Kinect != value)
+                {
+                    if (this._Kinect != null)
+                    {
+                        UninitializeKinectSensor(this._Kinect);
+                        this._Kinect = null;
+                    }
+                    if (value != null && value.Status == KinectStatus.Connected)
+                    {
+                        this._Kinect = value;
+                        InitializeKinectSensor(this._Kinect);
+                    }
+                }
+            }
+        }
+
+        private void UninitializeKinectSensor(KinectSensor kinectSensor)
+        {
+            if (kinectSensor != null)
+            {
+                kinectSensor.Stop();
+                kinectSensor.ColorFrameReady -= Kinect_ColorFrameReady;
+                kinectSensor.SkeletonFrameReady -= Kinect_SkeletonFrameReady;
+            }
+        }
+
+        private void InitializeKinectSensor(KinectSensor kinectSensor)
+        {
+            if (kinectSensor != null)
+            {
+                ColorImageStream colorStream = kinectSensor.ColorStream;
+                colorStream.Enable();
+                this._ColorImageBitmap = new WriteableBitmap(colorStream.FrameWidth, colorStream.FrameHeight,
+                    96, 96, PixelFormats.Bgr32, null);
+                this._ColorImageBitmapRect = new Int32Rect(0, 0, colorStream.FrameWidth, colorStream.FrameHeight);
+                this._ColorImageStride = colorStream.FrameWidth * colorStream.FrameBytesPerPixel;
+                videoStream.Source = this._ColorImageBitmap;
+
+                kinectSensor.SkeletonStream.Enable(new TransformSmoothParameters()
+                {
+                    Correction = 0.5f,
+                    JitterRadius = 0.05f,
+                    MaxDeviationRadius = 0.04f,
+                    Smoothing = 0.5f
+                });
+
+                kinectSensor.SkeletonFrameReady += Kinect_SkeletonFrameReady;
+                kinectSensor.ColorFrameReady += Kinect_ColorFrameReady;
+                kinectSensor.Start();
+                kinectSensor.SkeletonStream.TrackingMode = SkeletonTrackingMode.Seated;
+                this.FrameSkeletons = new Skeleton[this.Kinect.SkeletonStream.FrameSkeletonArrayLength];
+                /*this.interactionStream = new InteractionStream(kinectSensor, new DummyInteractionClient());
+                this.interactionStream.InteractionFrameReady += InteractionStreamOnInteractionFrameReady;*/
+            }
+        }
+        private void InteractionStreamOnInteractionFrameReady(object sender, InteractionFrameReadyEventArgs e)
+        {
+            using (InteractionFrame frame = e.OpenInteractionFrame())
+            {
+                if (frame != null)
+                {
+                    if (this.userInfos == null)
+                    {
+                        this.userInfos = new UserInfo[InteractionFrame.UserInfoArrayLength];
+                    }
+
+                    frame.CopyInteractionDataTo(this.userInfos);
+                }
+                else
+                {
+                    return;
+                }
+            }
+
+
+
+            foreach (UserInfo userInfo in this.userInfos)
+            {
+                foreach (InteractionHandPointer handPointer in userInfo.HandPointers)
+                {
+                    string action = null;
+
+                    switch (handPointer.HandEventType)
+                    {
+                        case InteractionHandEventType.Grip:
+                            action = "gripped";
+                            break;
+
+                        case InteractionHandEventType.GripRelease:
+                            action = "released";
+
+                            break;
+                    }
+
+                    if (action != null)
+                    {
+                        string handSide = "unknown";
+
+                        switch (handPointer.HandType)
+                        {
+                            case InteractionHandType.Left:
+                                handSide = "left";
+                                break;
+
+                            case InteractionHandType.Right:
+                                handSide = "right";
+                                break;
+                        }
+                        //comprobar si conincide con la mano primario y en caso de coincidir
+                        //hand.JointType == JointType.HandRight
+                        if (handSide == "left")
+                        {
+                            if (action == "released")
+                            {
+                                // left hand released code here
+                            }
+                            else
+                            {
+                                // left hand gripped code here
+                            }
+                        }
+                        else
+                        {
+                            if (action == "released")
+                            {
+                                // right hand released code here
+                            }
+                            else
+                            {
+                                // right hand gripped code here
+                            }
+                        }
+                    }
+                }
+            }
+        }
+
+
+        private void Kinect_ColorFrameReady(object sender, ColorImageFrameReadyEventArgs e)
+        {
+            using (ColorImageFrame frame = e.OpenColorImageFrame())
+            {
+                if (frame != null)
+                {
+                    byte[] pixelData = new byte[frame.PixelDataLength];
+                    frame.CopyPixelDataTo(pixelData);
+                    this._ColorImageBitmap.WritePixels(this._ColorImageBitmapRect, pixelData,
+                        this._ColorImageStride, 0);
+                }
+            }
+        }
+
+        private void Kinect_SkeletonFrameReady(object sender, SkeletonFrameReadyEventArgs e)
+        {
+            using (SkeletonFrame frame = e.OpenSkeletonFrame())
+            {
+                if (frame != null)
+                {
+                    frame.CopySkeletonDataTo(this.FrameSkeletons);
+                    Skeleton skeleton = GetPrimarySkeleton(this.FrameSkeletons);
+
+                    if (skeleton == null)
+                    {
+                        kinectButton.Visibility = Visibility.Collapsed;
+                    }
+                    else
+                    {
+                    
+                        Joint primaryHand = GetPrimaryHand(skeleton);
+                        TrackHand(primaryHand);
+                        
+                    }
+                }
+            }
+        }
+
+
+        //track and display hand
+        private void TrackHand(Joint hand)
+        {
+            if (hand.TrackingState == JointTrackingState.NotTracked)
+            {
+                kinectButton.Visibility = System.Windows.Visibility.Collapsed;
+            }
+            else
+            {
+                kinectButton.Visibility = System.Windows.Visibility.Visible;
+                
+                DepthImagePoint point = this.Kinect.MapSkeletonPointToDepth(hand.Position, DepthImageFormat.Resolution640x480Fps30);
+                handX = (int)((point.X * LayoutRoot.ActualWidth / this.Kinect.DepthStream.FrameWidth) -
+                    (kinectButton.ActualWidth / 2.0));
+                handY = (int)((point.Y * LayoutRoot.ActualHeight / this.Kinect.DepthStream.FrameHeight) -
+                    (kinectButton.ActualHeight / 2.0));
+                Canvas.SetLeft(kinectButton, handX);
+                Canvas.SetTop(kinectButton, handY);
+
+                if (isHandOver(kinectButton, buttons)) kinectButton.Hovering();
+                else kinectButton.Release();
+                if (!cuadroSeleccionado)
+                {
+                    if (hand.JointType == JointType.HandRight)
+                    {
+
+                        kinectButton.ImageSource = "/Images/RightHand.png";
+                        kinectButton.ActiveImageSource = "/Images/RightHand.png";
+                    }
+                    else
+                    {
+                        kinectButton.ImageSource = "/Images/LeftHand.png";
+                        kinectButton.ActiveImageSource = "/Images/LeftHand.png";
+                    }
+                }
+                else
+                {
+                    kinectButton.ImageSource = imagenMano;
+                    kinectButton.ActiveImageSource = imagenMano;
+                }
+                   
+
+            }
+        }
+
+        //detect if hand is overlapping over any button
+        private bool isHandOver(FrameworkElement hand, List<Button> buttonslist)
+        {
+            var handTopLeft = new System.Windows.Point(Canvas.GetLeft(hand), Canvas.GetTop(hand));
+            var handX = handTopLeft.X + hand.ActualWidth / 2;
+            var handY = handTopLeft.Y + hand.ActualHeight / 2;
+
+            foreach (Button target in buttonslist)
+            {
+                System.Windows.Point targetTopLeft = new System.Windows.Point(Canvas.GetLeft(target), Canvas.GetTop(target));
+                if (handX > targetTopLeft.X &&
+                    handX < targetTopLeft.X + target.Width &&
+                    handY > targetTopLeft.Y &&
+                    handY < targetTopLeft.Y + target.Height)
+                {
+                    selected = target;
+                    return true;
+                }
+            }
+            return false;
+        }
+
+        //get the hand closest to the Kinect sensor
+        private static Joint GetPrimaryHand(Skeleton skeleton)
+        {
+            Joint primaryHand = new Joint();
+            if (skeleton != null)
+            {
+                primaryHand = skeleton.Joints[JointType.HandLeft];
+                Joint rightHand = skeleton.Joints[JointType.HandRight];
+                if (rightHand.TrackingState != JointTrackingState.NotTracked)
+                {
+                    if (primaryHand.TrackingState == JointTrackingState.NotTracked)
+                    {
+                        primaryHand = rightHand;
+                    }
+                    else
+                    {
+                        if (primaryHand.Position.Z > rightHand.Position.Z)
+                        {
+                            primaryHand = rightHand;
+                        }
+                    }
+                }
+            }
+            return primaryHand;
+        }
+
+        //get the skeleton closest to the Kinect sensor
+        private static Skeleton GetPrimarySkeleton(Skeleton[] skeletons)
+        {
+            Skeleton skeleton = null;
+            if (skeletons != null)
+            {
+                for (int i = 0; i < skeletons.Length; i++)
+                {
+                    if (skeletons[i].TrackingState == SkeletonTrackingState.Tracked)
+                    {
+                        if (skeleton == null)
+                        {
+                            skeleton = skeletons[i];
+                        }
+                        else
+                        {
+                            if (skeleton.Position.Z > skeletons[i].Position.Z)
+                            {
+                                skeleton = skeletons[i];
+                            }
+                        }
+                    }
+                }
+            }
+            return skeleton;
+        }
+
+        void kinectButton_Click(object sender, RoutedEventArgs e)
+        {
+            selected.RaiseEvent(new RoutedEventArgs(Button.ClickEvent, selected));
+        }
+        private void arrastrarCuadro(object sender, RoutedEventArgs e)
+        {
+            label1.Content = "cuadro1";
+            imagenMano = "/Images/1.png";
+            cuadroSeleccionado = true;
+
+
+        }
+       
+
+         /*private void quitButton_Click(object sender, RoutedEventArgs e)
+         {
+             System.Windows.Application.Current.Shutdown();
+         }*/
+    }
+}
